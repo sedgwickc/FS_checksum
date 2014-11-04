20e3e52a6f7f32d8661445af9ebefffffc19f46b
Charles Sedgwick <sedgwickc@mymacewan.ca>
2014-11-03 22:11:49 -0700

    Create graph of checksumming test programs performance.
    Create bash script to run each test program ten times each on a range of data
    files. Add graph to report.

--- test/benchmark.sh
+++ test/benchmark.sh
@@ -2,28 +2,32 @@
 
 E_SSL="./checksum"
 E_SHA="./checksum_popen"
-E_MEM="./checksum_memap"
+E_MEM="./checksum_memmap"
 
 R_SSL="res_ssl"
 R_SHA1SUM="res_sha"
 R_MEMMAP="res_memmap"
 
-T_TEN="dat_files/ten_mb.dat"
-T_TWENTY="dat_files/twenty_mb.dat"
-T_EIGHTY="dat_files/eighty_mb.dat"
-T_120="dat_files/120_mb.dat"
+FILES=( "dat_files/one_mb.dat" 
+"dat_files/five_mb.dat" 
+"dat_files/ten_mb.dat" 
+"dat_files/twenty_mb.dat" 
+"dat_files/forty_mb.dat"
+"dat_files/eighty_mb.dat" 
+"dat_files/160_mb.dat" )
 
-SUM_SSL=0
-
-# find average run time over 1000 runs
-for i in {1..10}
+echo $E_SSL
+for file in "${FILES[@]}" 
 do
-	RES=$(/usr/bin/time -f %U $E_SSL $T_TEN)
-	SECONDS=0
-	$E_SSL $T_TEN
-	SUM_SSL=$(echo "$SUM_SSL + $SECONDS" | bc );
-	echo $SECONDS ", " $SUM_SSL
-
+	echo -n "$file: "
+	$E_SSL $file
+	
 done
 
-echo $SUM_SSL
+echo
+echo $E_MEM
+for file in "${FILES[@]}" 
+do
+	echo -n "$file: "
+	$E_MEM $file
+done
