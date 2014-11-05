add3ccbdcce388d6695331187f9fafcf39897a6c
Charles Sedgwick <sedgwickc@mymail.macewan.ca>
2014-11-05 13:35:22 -0700

    Minor changes to run_loop.sh

--- test/scripts/run_loop.sh
+++ test/scripts/run_loop.sh
@@ -5,14 +5,14 @@
 NUM=$2
 T=$1
 
+echo
 echo "Generating test config"
 echo "----------------------"
-echo
 ./gen_config.sh
 
+echo
 echo "Running FSchecksum $NUM times with $T threads"
 echo "----------------------"
-echo
 for num in $( seq $NUM ) 
 do
 	../../FSchecksum -n $T ../usr_bin_sums
